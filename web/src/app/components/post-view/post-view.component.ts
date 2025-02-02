f43ffd901c824e8598b72d5f9df994f99f734b77
DESKTOP-8CJ91S7\Bartek <bartek.37915@gmail.com>
2025-02-02 14:21:04 +0100

    Added new comment validation on Frontend

--- web/src/app/components/post-view/post-view.component.ts
+++ web/src/app/components/post-view/post-view.component.ts
@@ -1,40 +1,49 @@
-import {Component, OnInit} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {ActivatedRoute} from '@angular/router';
-import {CommentViewComponent} from '../comment-view/comment-view.component';
-import {NgForOf} from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { CommentViewComponent } from '../comment-view/comment-view.component';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-post-view',
   imports: [
-    FormsModule,
+    ReactiveFormsModule,
     CommentViewComponent,
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   templateUrl: './post-view.component.html',
   styleUrl: './post-view.component.css'
 })
 export class PostViewComponent implements OnInit {
-
-
   post = { title: 'Wprowadzenie do Termodynamiki', content: 'Podstawowy przewodnik po zasadach termodynamiki.',
     category: 'Termodynamika', user: { username: 'admin' },
-    likes: 0, comments: [{id: 1},
-      {id: 2}], createdAt: '2025-01-01 15:00' };
- newComment = { content: '' };
+    likes: 0, comments: [{id: 1}, {id: 2}], createdAt: '2025-01-01 15:00' };
+  commentForm: FormGroup;
 
- constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private fb: FormBuilder) {
+    this.commentForm = this.fb.group({
+      content: ['', [Validators.required, Validators.minLength(5)]]
+    });
+  }
+
   ngOnInit() {
     const postId = this.route.snapshot.paramMap.get('id');
-    // Fetch the post using the postId
     console.log('Post ID:', postId);
-  }
-  addComment(){
-    console.log('Komentarz został dodany. ' + this.newComment.content);
-    this.newComment = { content: '' };
+
+
   }
 
-  addLike(){
+  addComment() {
+    if (this.commentForm.valid) {
+      console.log('Komentarz został dodany. ' + this.commentForm.value.content);
+      this.commentForm.reset();
+    } else {
+      console.log('Formularz jest nieprawidłowy.');
+    }
+  }
+
+  addLike() {
     console.log('Polubienie zostało dodane.');
   }
 }
